ad15b1e14e065dec11132b5746195142fbe28783
sardar81574 <sardardhakad81@gmail.com>
2026-01-19 18:02:25 +0000

    conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,31 @@
+let score = "33abg"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" =>33
+//"33adsd" => NaN
+//true => 1 ; false => 0 
+
+let isLoggedIn ="sardar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1=> true ; 0 => false
+//"" => false 
+//"sardar" => true
+
+let Somenumber = 33
+
+
+let stringnumber = String(Somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
